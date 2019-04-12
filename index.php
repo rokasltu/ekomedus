76b23872378bd6a339a79961bedd2319c6026c7d
rokasltu <36819918+rokasltu@users.noreply.github.com>
2019-04-12 11:06:39 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,150 +1,148 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>EKOMEDUS | elektroninė medaus ir kitų bičių produktų parduotuvė</title>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+      <title>EKOMEDUS | elektroninė medaus ir kitų bičių produktų parduotuvė</title>
 
-  <!-- CSS  -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-</head>
+      <!-- CSS  -->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    </head>
 
-<body>
-  <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper"><image width="80px" src="pictures/logo.svg"></image>
-      <ul class="right">
-          <!--Nukreipimas i uzsakymo be registracijos forma-->
-        <li><a href="forma/public/forma.php">Užsisakykite be registracijos</a></li>
-          <!--Nukreipimas i prisijungimo / registracijos forma-->
-        <li><a href="login/login.php">Prisijunkite</a></li>
-      </ul>
+    <body>
+      <nav class="light-blue lighten-1" role="navigation">
+        <div class="nav-wrapper"><image width="80px" src="pictures/logo.svg"></image>
+          <ul class="right">
+              <!--Nukreipimas i uzsakymo be registracijos forma-->
+            <li><a href="forma/public/forma.php">Užsisakykite be registracijos</a></li>
+              <!--Nukreipimas i prisijungimo / registracijos forma-->
+            <li><a href="login/login.php">Prisijunkite</a></li>
+          </ul>
 
-    </div>
-  </nav>
-
-
-  <div class="container">
-    <div class="section">
-
-      <!--   Ikonu sekcija   -->
-      <div class="row">
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">Tik aukščiausia kokybė</h5>
-
-            <p class="light">Mes prekiaujame tik aukščiausios kokybės medumi. Bitynas yra įsukūręs Varėnos r., tarp pievų ir miškų</p>
-          </div>
         </div>
+      </nav>
 
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">Draugiški aplinkai</h5>
 
-            <p class="light">Mūsų bitynas yra labai ekologiškoje vietovėje, nutolęs nuo pagrindinių kelių, pievų ir miškų apsuptyje. Tai mus skatina taip pat būti draugiškais aplinkai - naudojame tik medinius avilius, medų sukame rankiniu būdu, pilstome tik į stiklinę tarą</p>
-          </div>
-        </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-            <h5 class="center">Svarbiausia Jūsų poreikiai</h5>
-
-            <p class="light">mes galime pasiūlyti ne tik kokybiškos medaus, bet ir kitų bičių produktų - pikio, pikio - vaško mišinio ir pan. Registruotiems vartotojams nuolat siūlome įvairias nuolaidas</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-  </div>
-        <script>
-    function neregNeveikia (){
-        prompt ("Siuo metu neveikia, pirkite be registracijos");
-    }
-
-    </script>
-    <!--produktu sarasas-->
-    <div class="container">
+      <div class="container">
         <div class="section">
-            <div class="row">
-    <div class="col s12 m4">
-        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>0.5 litro medaus stiklainis, 4,50 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
-    </div>
-    <div class="col s12 m4">
-        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>0,7 litro (1 kg.) medaus stiklainis, 6 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
-    </div>
-    <div class="col s12 m4">
-        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>1 litro medaus stiklainis, 9 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
-    </div>
-            </div>
-            <div class="row">
-    <div class="col s12 m4">
-        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>3 litrų medaus stiklainis, 25 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
-    </div>
-    </div>
-    </div>
-    </div>
 
+          <!--   Ikonu sekcija   -->
+          <div class="row">
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+                <h5 class="center">Tik aukščiausia kokybė</h5>
+
+                <p class="light">Mes prekiaujame tik aukščiausios kokybės medumi. Bitynas yra įsukūręs Varėnos r., tarp pievų ir miškų</p>
+              </div>
+            </div>
+
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+                <h5 class="center">Draugiški aplinkai</h5>
+
+                <p class="light">Mūsų bitynas yra labai ekologiškoje vietovėje, nutolęs nuo pagrindinių kelių, pievų ir miškų apsuptyje. Tai mus skatina taip pat būti draugiškais aplinkai - naudojame tik medinius avilius, medų sukame rankiniu būdu, pilstome tik į stiklinę tarą</p>
+              </div>
+            </div>
+
+            <div class="col s12 m4">
+              <div class="icon-block">
+                <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+                <h5 class="center">Svarbiausia Jūsų poreikiai</h5>
+
+                <p class="light">mes galime pasiūlyti ne tik kokybiškos medaus, bet ir kitų bičių produktų - pikio, pikio - vaško mišinio ir pan. Registruotiems vartotojams nuolat siūlome įvairias nuolaidas</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+            <script>
+                function neregNeveikia (){
+                prompt ("Siuo metu neveikia, pirkite be registracijos");
+                }
+            </script>
+        <!--produktu sarasas-->
+        <div class="container">
+            <div class="section">
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>0.5 litro medaus stiklainis, 4,50 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>0,7 litro (1 kg.) medaus stiklainis, 6 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>1 litro medaus stiklainis, 9 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
+                    </div>
+                </div>
+            <div class="row">
+                <div class="col s12 m4">
+                    <div class="icon-block"><img src="pictures/product1.jpeg" alt="1 kg. medaus stiklainis" class="text-bottom"><p>3 litrų medaus stiklainis, 25 EUR<br><button onclick="neregNeveikia()">Pirkti</button> </p></div>
+                </div>
+            </div>
+        </div>
+    </div>
         <footer class="page-footer orange">
             <div class="container">
                 <div class="row">
                     <div class="col l6 s12">
                         <h5 class="white-text">Ilonos Jucienės bitynas</h5>
                         <p class="grey-text text-lighten-4">Ilonos Jucienės bitynas, Varėnos r., bityno paso Nr. _____</p>
-                   <!--   Hey skaitliukas   -->
-                    <script language="javascript" type="text/javascript">
-                        <!--
-                        var _hey_lt_w = "", _hey_lt_h = "", _hey_lt_c = "";
-                        //-->
-                    </script>
-                    <script language="javascript1.2" type="text/javascript">
-                        <!--
-                        _hey_lt_w = screen.width; _hey_lt_h = screen.height; _hey_lt_c = navigator.appName.indexOf("Microsoft") >= 0 ? screen.colorDepth : screen.pixelDepth;
-                        //-->
-                    </script>
-                    <script language="javascript" type="text/javascript">
-                        <!--
-                        document.write("<a target='_blank' href='http://www.hey.lt/details.php?id=ekomedus'><img width='88' height='31' border='0' src='http://www.hey.lt/count.php?id=ekomedus&width=" + _hey_lt_w + "&height=" + _hey_lt_h + "&color=" + _hey_lt_c + "&referer=" + escape(document.referrer) + "' alt='Hey.lt - Nemokamas lankytojų skaitliukas'><\/a>");
-                        //-->
-                    </script>
-                    <noscript>
-                        <a target="_blank" href="http://www.hey.lt/details.php?id=ekomedus"><img width="88" height="31" border="0" src="http://www.hey.lt/count.php?id=ekomedus" alt="Hey.lt - Nemokamas lankytojų skaitliukas"></a>
-                    </noscript>
-        </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Mūsų partneriai</h5>
-                    <ul>
-                    <li><a class="white-text" href="http://www.vertybiumedis.lt">VšĮ "Vertybių medis"</a></li>
-                </ul>
+                        <!--   Hey skaitliukas   -->
+                        <script language="javascript" type="text/javascript">
+                            <!--
+                            var _hey_lt_w = "", _hey_lt_h = "", _hey_lt_c = "";
+                            //-->
+                        </script>
+                        <script language="javascript1.2" type="text/javascript">
+                            <!--
+                            _hey_lt_w = screen.width; _hey_lt_h = screen.height; _hey_lt_c = navigator.appName.indexOf("Microsoft") >= 0 ? screen.colorDepth : screen.pixelDepth;
+                            //-->
+                        </script>
+                        <script language="javascript" type="text/javascript">
+                            <!--
+                            document.write("<a target='_blank' href='http://www.hey.lt/details.php?id=ekomedus'><img width='88' height='31' border='0' src='http://www.hey.lt/count.php?id=ekomedus&width=" + _hey_lt_w + "&height=" + _hey_lt_h + "&color=" + _hey_lt_c + "&referer=" + escape(document.referrer) + "' alt='Hey.lt - Nemokamas lankytojų skaitliukas'><\/a>");
+                            //-->
+                        </script>
+                        <noscript>
+                            <a target="_blank" href="http://www.hey.lt/details.php?id=ekomedus"><img width="88" height="31" border="0" src="http://www.hey.lt/count.php?id=ekomedus" alt="Hey.lt - Nemokamas lankytojų skaitliukas"></a>
+                        </noscript>
             </div>
-            <div class="col l3 s12">
-                  <h5 class="white-text">Naudingos nuorodos</h5>
-                  <ul>
-                    <li><a class="white-text" href="https://www.facebook.com/groups/1443335525918870/?fref=nf">Bitininkų grupė Facebook'e</a></li>
-                    <li><a class="white-text" href="https://apis.lt/">Bičių pasaulis</a></li>
-                    <li><a class="white-text" href="http://www.bitinelis.lt/">Bitinėlis</a></li>
-                    <li><a class="white-text" href="http://www.biciukorys.lt/">Bičių korys</a></li>
-                  </ul>
+                <div class="col l3 s12">
+                    <h5 class="white-text">Mūsų partneriai</h5>
+                        <ul>
+                        <li><a class="white-text" href="http://www.vertybiumedis.lt">VšĮ "Vertybių medis"</a></li>
+                    </ul>
+                </div>
+                    <div class="col l3 s12">
+                      <h5 class="white-text">Naudingos nuorodos</h5>
+                      <ul>
+                        <li><a class="white-text" href="https://www.facebook.com/groups/1443335525918870/?fref=nf">Bitininkų grupė Facebook'e</a></li>
+                        <li><a class="white-text" href="https://apis.lt/">Bičių pasaulis</a></li>
+                        <li><a class="white-text" href="http://www.bitinelis.lt/">Bitinėlis</a></li>
+                        <li><a class="white-text" href="http://www.biciukorys.lt/">Bičių korys</a></li>
+                      </ul>
+                    </div>
+                </div>
             </div>
+        <div class="footer-copyright">
+          <div class="container">
+          Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+          </div>
         </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-      </div>
-    </div>
-  </footer>
+    </footer>
 
 
 
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
+      <!--  Scripts-->
+      <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="js/materialize.js"></script>
+      <script src="js/init.js"></script>
 
-  </body>
+      </body>
 </html>
